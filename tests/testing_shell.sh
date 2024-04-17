6d4101f74524c78963b418c420500af6400180aa
Pranav Verma <pverma@mathworks.com>
2024-04-17 15:30:37 +0530

    adding the shunit var

--- tests/testing_shell.sh
+++ tests/testing_shell.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-SHUNIT2_PATH="${GITHUB_WORKSPACE}/shunit2/shunit2"
 
 testOptionalUserCommandNotSet() {
     unset OPTIONAL_USER_COMMAND
 
-    result=$(${GITHUB_WORKSPACE}/tests/optional_user_command.sh | tr '\n' ' ')
+    result=$(tests/optional_user_command.sh | tr '\n' ' ')
+
     expectedOutput="Optional user command - script started. No optional user command was passed. Optional user command - script completed. "
 
     echo "Expected: '$expectedOutput'"
@@ -18,7 +18,7 @@ testOptionalUserCommandIsSet() {
     OPTIONAL_USER_COMMAND='echo "Hello, World!"'
 
     # Execute the script in a subshell to keep the environment variable local
-    result=$(OPTIONAL_USER_COMMAND="$OPTIONAL_USER_COMMAND" ${GITHUB_WORKSPACE}/tests/optional_user_command.sh | tr '\n' ' ')
+    result=$(OPTIONAL_USER_COMMAND="$OPTIONAL_USER_COMMAND" tests/optional_user_command.sh | tr '\n' ' ')
 
     # Define expected output
     expectedOutput="Optional user command - script started. The passed string is an inline shell command. Hello, World! Optional user command - script completed. "
